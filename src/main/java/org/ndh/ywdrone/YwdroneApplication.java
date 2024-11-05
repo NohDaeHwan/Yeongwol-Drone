9afd2458d77de47863b1a69e103189d5049be4bd
노대환 <eoghks5953@ejbt.co.kr>
2024-11-05 18:50:34 +0900

    Initial commit

---
+++ src/main/java/org/ndh/ywdrone/YwdroneApplication.java
@@ -0,0 +1,13 @@
+package org.ndh.ywdrone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YwdroneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YwdroneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ndh/ywdrone/YwdroneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ndh.ywdrone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YwdroneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
